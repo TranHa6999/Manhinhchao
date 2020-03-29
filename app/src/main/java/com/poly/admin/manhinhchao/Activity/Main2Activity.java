dfffcca925f9b60035d7a5362d01f934e149f5c2
TranHa6999 <tranha6999@gmail.com>
2020-03-29 16:48:38 +0700

    Comiitttt

---
+++ app/src/main/java/com/poly/admin/manhinhchao/Activity/Main2Activity.java
@@ -0,0 +1,16 @@
+package com.poly.admin.manhinhchao.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.poly.admin.manhinhchao.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
